f79d00d52f0f8d3c6957879ddc06b0e221551414
mabn1 <mababativan@udistrital.edu.co>
2026-05-02 22:01:35 -0500

    Se logra el ejercicio 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,7 +10,12 @@ const Content = (props) => {
   return(
     <p>{props.part} {props.exercises}</p>
   )
+}
 
+const Total = (props) => {
+  return(
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
 }
 
 const App = () => {
@@ -28,6 +33,7 @@ const App = () => {
       <Content part={part1} exercises={exercises1}/>
       <Content part={part2} exercises={exercises2}/>
       <Content part={part3} exercises={exercises3}/>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
