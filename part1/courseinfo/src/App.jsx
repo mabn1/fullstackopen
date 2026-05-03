8d2001054803709091d408c36e437cb5fe3781b5
mabn1 <mababativan@udistrital.edu.co>
2026-05-03 00:41:52 -0500

    Este es el ejercicio 1.3 usando objetos y funciones

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,41 +1,46 @@
-  const Header = (props) => {
-    return(
-      <div>
-        <h1>{props.course}</h1>
-      </div>
-    )
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+
+}
+
+const Total = (props) => {
+  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
   }
 
-  const Content = (props) => {
-    return(
-      <p>{props.part} {props.exercises}</p>
-    )
-  }
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+    </div>
+  )
+}
 
-  const Total = (props) => {
-    return(
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    )
-  }
-
-  const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-
-    return (
-      <div>
-        <Header course={course}/>
-        <Content part={part1} exercises={exercises1}/>
-        <Content part={part2} exercises={exercises2}/>
-        <Content part={part3} exercises={exercises3}/>
-        <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      </div>
-    )
-  }
-
-  export default App  
+export default App  
