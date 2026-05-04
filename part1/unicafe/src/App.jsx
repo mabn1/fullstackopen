df8cd01aa078b18d56883100ed90d70f45931e05
mabn1 <mababativan@udistrital.edu.co>
2026-05-03 21:38:10 -0500

    ejercicio 1.9 solucionado

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,17 +6,23 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
-const Statistics = ({ good, neutral, bad }) => (
-  <div>
-    <h1>statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {good + neutral + bad}</p>
-    <p>average {((good + (neutral * 0) + (bad * -1)) / (good + neutral + bad))}</p>
-    <p>positive {(good / (good + neutral + bad)) * 100} %</p>
-  </div>
-)
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return <p>No feedback given</p>
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {(good / (good + neutral + bad)) * 100} %</p>
+    </div>
+  )
+}
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
