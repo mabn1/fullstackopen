078bc4e88df5ab7289e70ec3961b88296c9037ba
mabn1 <mababativan@udistrital.edu.co>
2026-05-07 17:31:20 -0500

    ejercicio 2.9: se agrega la funcionalidad de busqueda de contactos

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('') // 👈 NUEVO
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -11,6 +18,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value) // 👈 NUEVO
   }
 
   const addPerson = (event) => {
@@ -26,9 +37,22 @@ const App = () => {
     setNewNumber('')
   }
 
+  // 👇 FILTRO
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      {/* 👇 INPUT DE FILTRO */}
+      <div>
+        filter shown with{' '}
+        <input value={filter} onChange={handleFilterChange} />
+      </div>
+
+      <h2>Add a new</h2>
 
       <form onSubmit={addPerson}>
         <div>
@@ -44,7 +68,7 @@ const App = () => {
 
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => (
+        {personsToShow.map((person, index) => ( // 👈 CAMBIO AQUÍ
           <li key={index}>
             {person.name} {person.number}
           </li>
