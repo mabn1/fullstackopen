497b453d648d9ef3fe61b8f6d460ba3143e03179
mabn1 <mababativan@udistrital.edu.co>
2026-05-07 20:24:40 -0500

    ejercicio 2.12: se guardan los contactos usando json-server GET y POST con axios

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,43 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Filter = ({ filter, handleFilterChange }) => (
-  <div>
-    filter shown with{' '}
-    <input value={filter} onChange={handleFilterChange} />
-  </div>
-)
-
-const PersonForm = ({
-  addPerson,
-  newName,
-  handleNameChange,
-  newNumber,
-  handleNumberChange
-}) => (
-  <form onSubmit={addPerson}>
-    <div>
-      name: <input value={newName} onChange={handleNameChange} />
-    </div>
-    <div>
-      number: <input value={newNumber} onChange={handleNumberChange} />
-    </div>
-    <div>
-      <button type="submit">add</button>
-    </div>
-  </form>
-)
-
-const Persons = ({ personsToShow }) => (
-  <ul>
-    {personsToShow.map(person => (
-      <li key={person.id}>
-        {person.name} {person.number}
-      </li>
-    ))}
-  </ul>
-)
-
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
@@ -52,31 +15,26 @@ const App = () => {
       })
   }, [])
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
-
   const addPerson = (event) => {
     event.preventDefault()
 
     const newPerson = {
       name: newName,
-      number: newNumber,
-      id: persons.length + 1
+      number: newNumber
     }
 
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-    setNewNumber('')
+    axios
+      .post('http://localhost:3001/persons', newPerson)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
   }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
 
   const personsToShow = persons.filter(person =>
     person.name.toLowerCase().includes(filter.toLowerCase())
@@ -86,23 +44,35 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
 
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <div>
+        filter shown with{' '}
+        <input value={filter} onChange={handleFilterChange} />
+      </div>
 
       <h3>Add a new</h3>
 
-      <PersonForm
-        addPerson={addPerson}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-      />
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
 
       <h3>Numbers</h3>
-
-      <Persons personsToShow={personsToShow} />
+      <ul>
+        {personsToShow.map(person => (
+          <li key={person.id}>
+            {person.name} {person.number}
+          </li>
+        ))}
+      </ul>
     </div>
   )
-} 
+}
 
 export default App
