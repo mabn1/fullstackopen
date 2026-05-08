06c4569cafc830f0de95124c374ea43bbbac93a2
mabn1 <mababativan@udistrital.edu.co>
2026-05-08 18:11:08 -0500

    ejrecicio 2.19: Se agrega el botón para seleccionar un pais en el listado

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -6,6 +6,7 @@ import CountryDetail from './components/CountryDetail'
 const App = () => {
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState('')
+  const [selectedCountry, setSelectedCountry] = useState(null)
 
   useEffect(() => {
     countryService.getAll().then(data => setCountries(data))
@@ -13,26 +14,36 @@ const App = () => {
 
   const handleSearchChange = (event) => {
     setSearch(event.target.value)
+    setSelectedCountry(null)
+  }
+
+  const handleShow = (country) => {
+    setSelectedCountry(country)
   }
 
   const filtered = countries.filter(country =>
     country.name.common.toLowerCase().includes(search.toLowerCase())
   )
 
+  const countriesToShow = selectedCountry ? [selectedCountry] : filtered
+
   return (
     <div>
       <div>
-        find countries <input value={search} onChange={handleSearchChange} />
+        find countries{' '}
+        <input value={search} onChange={handleSearchChange} />
       </div>
 
-      {filtered.length > 10 && <p>Too many matches, specify another filter</p>}
-
-      {filtered.length <= 10 && filtered.length > 1 && (
-        <CountriesList countries={filtered} />
+      {!selectedCountry && filtered.length > 10 && (
+        <p>Too many matches, specify another filter</p>
       )}
 
-      {filtered.length === 1 && (
-        <CountryDetail country={filtered[0]} />
+      {!selectedCountry && filtered.length <= 10 && filtered.length > 1 && (
+        <CountriesList countries={filtered} onShow={handleShow} />
+      )}
+
+      {countriesToShow.length === 1 && (
+        <CountryDetail country={countriesToShow[0]} />
       )}
     </div>
   )
--- part2/countries/src/components/CountriesList.jsx
+++ part2/countries/src/components/CountriesList.jsx
@@ -1,9 +1,15 @@
-const CountriesList = ({ countries }) => {
+const CountriesList = ({ countries, onShow }) => {
   return (
     <ul>
       {countries.map(country => (
         <li key={country.cca3}>
           {country.name.common}
+          <button
+            onClick={() => onShow(country)}
+            style={{ marginLeft: '10px' }}
+          >
+            show
+          </button>
         </li>
       ))}
     </ul>
