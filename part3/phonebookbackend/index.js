cd5821848a6fbb93cbecd4ffb4b7976b61d2fb6d
mabn1 <mababativan@udistrital.edu.co>
2026-05-09 14:19:27 -0500

    ejercicio 3.8: se añaden datos de petición post con morgan

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -3,7 +3,10 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
