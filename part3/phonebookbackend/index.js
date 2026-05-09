132d8a3cda98d8f47a97d145afde50ba65569b6d
mabn1 <mababativan@udistrital.edu.co>
2026-05-09 11:23:49 -0500

    ejercicio 3.5: se añaden nuevos contactos con POST y math random para generar un ID alteatorio

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
   { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
   { id: 3, name: "Dan Abramov", number: "12-43-234345" },
   { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" }
 ]
+
+const generateId = () => Math.floor(Math.random() * 1000000)
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
@@ -25,10 +29,22 @@ app.get('/api/persons/:id', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-
   persons = persons.filter(p => p.id !== id)
-
   res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  const newPerson = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(newPerson)
+
+  res.json(newPerson)
 })
 
 app.get('/info', (req, res) => {
