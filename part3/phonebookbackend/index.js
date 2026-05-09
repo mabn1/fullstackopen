90e0a1238389093b1f2cf8808a51b48953388324
mabn1 <mababativan@udistrital.edu.co>
2026-05-09 18:56:54 -0500

    build: se agrega frontend compilado para producción

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
-morgan.token('body', (req) => JSON.stringify(req.body))
-
+morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use(express.static('dist'))
 
 let persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
@@ -73,7 +76,6 @@ app.get('/info', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3001
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
