fba54c34fe5fdf73f0a1a21aed096ededfbc46b9
mabn1 <mababativan@udistrital.edu.co>
2026-05-15 11:44:12 -0500

    ejercicio 4.15: Creación de usuarios con post

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => logger.info('connected to MongoDB'))
@@ -15,9 +16,9 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
-// middleware al final SIEMPRE
 app.use(middleware.errorHandler)
 
 module.exports = app
