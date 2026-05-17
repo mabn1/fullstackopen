7ad559c26e95b60dba5cb7816261995b9da5dfc2
mabn1 <mababativan@udistrital.edu.co>
2026-05-17 07:20:36 -0500

    ejercicio 5.14: test de visualización de likes y url con el boton de view

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
-import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
+import Blog from './Blog'
+
 
 test('renders title and author, but not url or likes by default', () => {
   const blog = {
@@ -12,9 +13,37 @@ test('renders title and author, but not url or likes by default', () => {
   }
 
   render(<Blog blog={blog} />)
-  // visibles por defecto
+
   expect(screen.getByText('Testing React apps Miguel')).toBeDefined()
 
   expect(screen.queryByText('http://example.com')).toBeNull()
   expect(screen.queryByText('likes 10')).toBeNull()
+})
+
+test('when view button is clicked, url and likes are shown', async () => {
+  const blog = {
+    title: 'Testing React apps',
+    author: 'Miguel',
+    url: 'http://example.com',
+    likes: 10,
+    user: {
+      username: 'miguel',
+      name: 'Miguel'
+    }
+  }
+
+  const user = {
+    username: 'miguel',
+    name: 'Miguel'
+  }
+
+  render(<Blog blog={blog} user={user} />)
+
+  const userEventInstance = userEvent.setup()
+
+  const viewButton = screen.getByText('view')
+  await userEventInstance.click(viewButton)
+
+  expect(screen.getByText('http://example.com')).toBeDefined()
+  expect(screen.getByText('likes 10')).toBeDefined()
 })
