e14f5e8e290dca73bf9e8b9fff71f875d5ebef48
mabn1 <mababativan@udistrital.edu.co>
2026-05-16 19:28:59 -0500

    ejercicio 5.7: Los blogs se despliegan solo con boton

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,48 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (!visible) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>hide</button>
+      </div>
+
+      <div>{blog.url}</div>
+
+      <div>
+        likes {blog.likes}
+        <button>like</button>
+      </div>
+
+      <div>
+        {blog.user?.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
