61c584fbb5396fd435e4923018ce127ca8cfe702
mabn1 <mababativan@udistrital.edu.co>
2026-05-17 08:58:06 -0500

    ejercicio 5.15: test de doble click en like verifica llamadas del handler

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -2,12 +2,11 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-
 test('renders title and author, but not url or likes by default', () => {
   const blog = {
     title: 'Testing React apps',
     author: 'Miguel',
-    url: 'http://example.com',
+    url: 'https://test.com',
     likes: 10,
     user: { name: 'Miguel' }
   }
@@ -16,34 +15,51 @@ test('renders title and author, but not url or likes by default', () => {
 
   expect(screen.getByText('Testing React apps Miguel')).toBeDefined()
 
-  expect(screen.queryByText('http://example.com')).toBeNull()
+  expect(screen.queryByText('https://test.com')).toBeNull()
   expect(screen.queryByText('likes 10')).toBeNull()
 })
 
-test('when view button is clicked, url and likes are shown', async () => {
+test('when view is clicked, url and likes are shown', async () => {
   const blog = {
     title: 'Testing React apps',
     author: 'Miguel',
-    url: 'http://example.com',
+    url: 'https://test.com',
     likes: 10,
-    user: {
-      username: 'miguel',
-      name: 'Miguel'
-    }
+    user: { name: 'Miguel' }
   }
 
-  const user = {
-    username: 'miguel',
-    name: 'Miguel'
+  const user = userEvent.setup()
+
+  render(<Blog blog={blog} />)
+
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(screen.getByText('https://test.com')).toBeDefined()
+  expect(screen.getByText('likes 10')).toBeDefined()
+})
+
+test('like button is called twice when clicked twice', async () => {
+  const blog = {
+    title: 'Testing React apps',
+    author: 'Miguel',
+    url: 'https://test.com',
+    likes: 10,
+    user: { name: 'Miguel' }
   }
 
-  render(<Blog blog={blog} user={user} />)
+  const mockHandler = vi.fn()
+  const user = userEvent.setup()
 
-  const userEventInstance = userEvent.setup()
+  render(<Blog blog={blog} handleLike={mockHandler} />)
 
   const viewButton = screen.getByText('view')
-  await userEventInstance.click(viewButton)
+  await user.click(viewButton)
 
-  expect(screen.getByText('http://example.com')).toBeDefined()
-  expect(screen.getByText('likes 10')).toBeDefined()
+  const likeButton = screen.getByText('like')
+
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
