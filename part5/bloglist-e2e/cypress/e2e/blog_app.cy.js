d43adb8a65f8faacb38a945fc73e11a6d5ca74bc
mabn1 <mababativan@udistrital.edu.co>
2026-05-19 18:27:04 -0500

    ejercicio 5.18 cypress: pruebas de login en cypress funcionando, se agrega reset de base de datos y validación de credenciales correctas e incorrectas

--- part5/bloglist-e2e/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-e2e/cypress/e2e/blog_app.cy.js
@@ -1,11 +1,46 @@
 describe('Blog app', function() {
 
   beforeEach(function() {
+
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+
+    const user = {
+      name: 'Test User',
+      username: 'testuser',
+      password: 'testpass'
+    }
+
+    cy.request('POST', 'http://localhost:3001/api/users', user)
+
     cy.visit('/')
   })
 
   it('Login form is shown', function() {
     cy.contains('Log in')
+  })
+
+  describe('Login', function() {
+
+    it('succeeds with correct credentials', function() {
+
+      cy.get('[data-testid="username"]').type('testuser')
+      cy.get('[data-testid="password"]').type('testpass')
+
+      cy.contains('login').click()
+
+      cy.contains('logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+
+      cy.get('[data-testid="username"]').type('testuser')
+      cy.get('[data-testid="password"]').type('wrongpass')
+
+      cy.contains('login').click()
+
+      cy.contains('wrong username or password')
+    })
+
   })
 
 })
