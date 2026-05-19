c8e80a38d3935b69ee4980ba51f59ad1bb1be565
mabn1 <mababativan@udistrital.edu.co>
2026-05-19 18:55:23 -0500

    ejercicio 5.20 Cypress: prueba de likes funcionando, se mejora estabilidad usando selectores más específicos y regex para evitar conflictos entre like y likes

--- part5/bloglist-e2e/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-e2e/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,6 @@
-describe('Blog app', function() {
+describe('Blog app', function () {
 
-  beforeEach(function() {
+  beforeEach(function () {
 
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
 
@@ -15,13 +15,13 @@ describe('Blog app', function() {
     cy.visit('/')
   })
 
-  it('Login form is shown', function() {
+  it('Login form is shown', function () {
     cy.contains('Log in')
   })
 
-  describe('Login', function() {
+  describe('Login', function () {
 
-    it('succeeds with correct credentials', function() {
+    it('succeeds with correct credentials', function () {
 
       cy.get('[data-testid="username"]').type('testuser')
       cy.get('[data-testid="password"]').type('testpass')
@@ -31,7 +31,7 @@ describe('Blog app', function() {
       cy.contains('logged in')
     })
 
-    it('fails with wrong credentials', function() {
+    it('fails with wrong credentials', function () {
 
       cy.get('[data-testid="username"]').type('testuser')
       cy.get('[data-testid="password"]').type('wrongpass')
@@ -43,9 +43,9 @@ describe('Blog app', function() {
 
   })
 
-    describe('When logged in', function() {
+  describe('When logged in', function () {
 
-    beforeEach(function() {
+    beforeEach(function () {
 
       cy.request('POST', 'http://localhost:3001/api/login', {
         username: 'testuser',
@@ -58,7 +58,7 @@ describe('Blog app', function() {
       })
     })
 
-    it('A blog can be created', function() {
+    it('A blog can be created', function () {
 
       cy.contains('create new blog').click()
 
@@ -69,6 +69,27 @@ describe('Blog app', function() {
       cy.get('#create-blog-button').click()
 
       cy.contains('Cypress blog Miguel')
+    })
+
+    it('A blog can be liked', function () {
+
+      cy.contains('create new blog').click()
+
+      cy.get('[placeholder="title"]').type('Blog con likes')
+      cy.get('[placeholder="author"]').type('Miguel')
+      cy.get('[placeholder="url"]').type('http://likes.com')
+
+      cy.get('#create-blog-button').click()
+
+      cy.contains('Blog con likes Miguel')
+
+      cy.contains('view').click()
+
+      cy.contains('likes 0')
+
+      cy.contains(/^like$/).click()
+
+      cy.contains(/^likes 1$/)
     })
 
   })
