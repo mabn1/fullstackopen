5f93cb58ac07130edd7003536ff0e86fb706b341
mabn1 <mababativan@udistrital.edu.co>
2026-05-19 23:31:06 -0500

    ejercicio 6.1: unicafe con redux funcionando

---
+++ part6/unicafe-redux/src/App.jsx
@@ -0,0 +1,29 @@
+const App = ({ store }) => {
+  const state = store.getState()
+
+  return (
+    <div>
+      <button onClick={() => store.dispatch({ type: 'GOOD' })}>
+        good
+      </button>
+
+      <button onClick={() => store.dispatch({ type: 'OK' })}>
+        ok
+      </button>
+
+      <button onClick={() => store.dispatch({ type: 'BAD' })}>
+        bad
+      </button>
+
+      <button onClick={() => store.dispatch({ type: 'ZERO' })}>
+        reset stats
+      </button>
+
+      <div>good {state.good}</div>
+      <div>ok {state.ok}</div>
+      <div>bad {state.bad}</div>
+    </div>
+  )
+}
+
+export default App
---
+++ part6/unicafe-redux/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import counterReducer from './reducers/counterReducer'
+import App from './App.jsx'
+
+const store = createStore(counterReducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App store={store} />)
+}
+
+renderApp()
+store.subscribe(renderApp)
